699040c53e7fd628754e974990ffbcc7a209b021
jooper <282376402@163.com>
2021-04-26 19:21:41 +0800

    调整官网首页

--- src/components/views/index.vue
+++ src/components/views/index.vue
@@ -1,23 +1,25 @@
 <template>
   <div>    
-   
+      
+
         <section class="first-section">
           <div class="container">
             <div class="row">
               <div class="col-md-12">
-                <div class="text-content">                             
+                <div class="text-content">           
+                  <!-- <h2><img src="../../assets/img/logo.png" alt="" style="width: 80px;height: 80px"/></h2> -->
                   <h2><font style="color: red;">觅</font>食</h2>
                   <div class="line-dec"></div>
                   <!-- <span>让每个商家都有大众能看到的潮品 &amp; </span> -->               
-                  <span>民以食为天，食以美为佳；觅食--分享身边美好的味道！</span>
+                  <span>民以食为天，食以佳为美；觅食--传递身边美好的味道！</span>
                 </div>
               </div>
             </div>
           </div>
         </section>
+        
 
-
-      
+        
 
         <section class="second-section">
           <div class="container">
@@ -28,7 +30,7 @@
                     <img src="../../assets/img/dy/dy.png" alt="" style="width: 90px;height: 90px;">
                   </div>
                   <h4>基于抖音平台</h4>
-                  <p>进行商家宣传短视频传播，在海量用户中，获得快速的曝光,快速获得精准用户</p>
+                  <p>传播商家宣传短视频，基于海量精准用户获得快速曝光</p>
                 </div>
               </div>
               <div class="col-md-3 col-sm-6">
@@ -36,8 +38,8 @@
                   <div class="icon">
                     <img src="../../assets/img//dy/rq.png" alt="" style="width: 90px;height: 90px;">
                   </div>
-                  <h4>精准定位客群</h4>
-                  <p>通过抖音用户裂变分享，在相似用户间传播</p>
+                  <h4>定位精准客群</h4>
+                  <p>通过用户分享，在相似群体间传播</p>
                 </div>
               </div>
               <div class="col-md-3 col-sm-6">
@@ -45,8 +47,8 @@
                   <div class="icon">
                     <img src="../../assets/img/dy/dpbq.png" alt="" style="width: 90px;height: 90px;">
                   </div>
-                  <h4>带店定位标签</h4>
-                  <p>分享的视频信息，带有商家精准定位标签信息，海量抖音用户可以准确的定位到商家位置</p>
+                  <h4>标记商家位置</h4>
+                  <p>分享的视频信息，带有商家精准位置信息，使潜在客户可以准确的获得商家的准确位置</p>
                 </div>  
               </div>
               <div class="col-md-3 col-sm-6">
@@ -54,8 +56,8 @@
                   <div class="icon">
                     <img src="../../assets/img/dy/fx.png" alt="" style="width: 90px;height: 90px;">
                   </div>
-                  <h4>一键发布</h4>
-                  <p>客人用抖音扫码一键分享，无需拍摄，提高传播效率</p>
+                  <h4>一键发布商家宣传视频</h4>
+                  <p>无需用户拍摄，便可一键发送商家准备好的宣传视频信息，提高传播效率</p>
                 </div>
               </div>
             </div>
@@ -63,14 +65,12 @@
         </section>
 
 
-
-          
         <section class="third-section">
           <div class="container">
             <div class="row">
               <div class="col-md-10 col-md-offset-1">
                 <div class="left-image col-md-4">
-                  <!-- <img src="img/left-image.png" alt=""> -->
+                  <!-- <img src="../../assets/img/left-image.png" alt=""> -->
                   <img src="../../assets/img/splb.png" alt="">
                 </div>
                 <div class="right-text col-md-8">
@@ -84,15 +84,13 @@
         </section>
 
 
-
-        
         <section class="second-section">
           <div class="container">
             <div class="row">
               <div class="col-md-3 col-sm-6">
                 <div class="service-item">
                   <div class="icon">
-                    <img src="../../assets/img/dy/sm.png" alt="" style="width: 128px;height: 128px;">
+                    <img src="../../assets/img/dy/sm.png" alt="" style="width: 90px;height: 90px;">
                   </div>
                   <h4>扫码</h4>
                   <p>客人用抖音APP扫描商家抖音二维码.</p>
@@ -101,25 +99,25 @@
               <div class="col-md-3 col-sm-6">
                 <div class="service-item">
                   <div class="icon">
-                    <img src="../../assets/img/dy/spzf.png" alt="" style="width: 128px;height: 128px;">
+                    <img src="../../assets/img/dy/spzf.png" alt="" style="width: 90px;height: 90px;">
                   </div>
                   <h4>自动转发短视频</h4>
-                  <p>客人扫描后，弹出相关商家优惠信息，点击领取将，自动转发商家宣传短视频到自己的抖音账户中</p>
+                  <p>扫描后，弹出相关商家优惠信息，点击领取将，自动转发商家宣传短视频到用户的抖音账户中</p>
                 </div>
               </div>
               <div class="col-md-3 col-sm-6">
                 <div class="service-item">
                   <div class="icon">
-                    <img src="../../assets/img/dy/zdbg.png" alt="" style="width: 128px;height: 128px;">
+                    <img src="../../assets/img/dy/zdbg.png" alt="" style="width: 90px;height: 90px;">
                   </div>
                   <h4>自动曝光</h4>
-                  <p>每个客转发的短视频在抖音平台中，最低曝光300~500次;如2个客人视频将曝光600~1000次，以此内推</p>
+                  <p>每个客人转发的短视频在抖音平台中，最低曝光300~500次;如2个客人视频将曝光600~1000次，以此类推</p>
                 </div>  
               </div>
               <div class="col-md-3 col-sm-6">
                 <div class="service-item">
                   <div class="icon">
-                    <img src="../../assets/img/dy/jzyh.png" alt="" style="width: 128px;height: 128px;">
+                    <img src="../../assets/img/dy/jzyh.png" alt="" style="width: 90px;height: 90px;">
                   </div>
                   <h4>带来精准客源</h4>
                   <p>客人的周边人群将发现店家的宣传视频，获取更多的潜在精准客户</p>
@@ -132,9 +130,6 @@
 
 
 
-
-      
-
         <section class="third-section">
           <div class="container">
             <div class="row">
@@ -143,8 +138,8 @@
                   <img src="../../assets/img/left-image1.png" alt="">
                 </div>
                 <div class="right-text col-md-8">
-                  <h4><em>店家条件</em><br>-需有自己的宣传短视频</h4>
-                  <p>店家可以用自己也有的短视频，或者我司专门定制化短视频。 定制流程如下</p>
+                  <h4><em>商家条件</em><br>-需有宣传短视频</h4>
+                  <p>店家可自行拍摄宣传短视频，也可以由本平台提供定制化短视频； 定制流程如下</p>
                 </div>
               </div>
             </div>
@@ -158,7 +153,7 @@
               <div class="col-md-3 col-sm-6">
                 <div class="service-item">
                   <div class="icon">
-                    <img src="../../assets/img/dy/yypz.png" alt="">
+                    <img src="../../assets/img/dy/yypz.png" alt="" style="width: 90px;height: 90px;">
                   </div>
                   <h4>预约拍摄</h4>
                   <p>商家和本平台预约到店进行宣传短视频拍摄时间.</p>
@@ -167,7 +162,7 @@
               <div class="col-md-3 col-sm-6">
                 <div class="service-item">
                   <div class="icon">
-                    <img src="../../assets/img/dy/ddps2.png" alt="">
+                    <img src="../../assets/img/dy/ddps2.png" alt="" style="width: 90px;height: 90px;">
                   </div>
                   <h4>到店拍摄</h4>
                   <p>专业团队到商家现场进行拍摄、取材</p>
@@ -176,7 +171,7 @@
               <div class="col-md-3 col-sm-6">
                 <div class="service-item">
                   <div class="icon">
-                    <img src="../../assets/img/dy/spjj.png" alt="">
+                    <img src="../../assets/img/dy/spjj.png" alt="" style="width: 90px;height: 90px;">
                   </div>
                   <h4>视频制作</h4>
                   <p>利用AI技术智能制作宣传短视频</p>
@@ -185,7 +180,7 @@
               <div class="col-md-3 col-sm-6">
                 <div class="service-item">
                   <div class="icon">
-                    <img src="../../assets/img/dy/scewm.png" alt="">
+                    <img src="../../assets/img/dy/scewm.png" alt="" style="width: 90px;height: 90px;">
                   </div>
                   <h4>生成店铺二维码</h4>
                   <p>生成具有店铺特色的二维码</p>
@@ -196,20 +191,14 @@
         </section>
 
 
-        
-
-
-
-        
         <footer>
           <div class="container">
             <div class="row">
-              <div class="col-md-12">
-
+              <div class="col-md-12">     
                 <ul>
-                  <a href="#"><li >微信：jootey</li></a>
+                  <li style="color:white">微信：jootey</li>
                 </ul>
-                <p>Copyright &copy; 2021  |   备案号:蜀ICP备2021008995号-1</p>
+                <p>Copyright &copy; 2021  | 蜀ICP备2021008995号-1 | 狸猫互动科技成都有限公司</p>
               </div>
             </div>
           </div>
