42cf3c1afb89e813819b2eb1d9ea67ca134ac3df
jooper <jootey1314@gmail.com>
2021-04-28 01:15:59 +0800

    cleanup code

--- src/router/index.js
+++ src/router/index.js
@@ -42,7 +42,8 @@ const routes = [
 ]
 
 const router = new Router({
-    mode: 'history',
+    // mode: 'history',
+    // base: '/platform',
     routes,
     linkActiveClass: 'active',
     linkExactActiveClass: 'active'
