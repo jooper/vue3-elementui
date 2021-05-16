ecfcfb247a790bd6f5ae017bbe23ba3e0fb84797
jooper <jootey1314@gmail.com>
2021-05-16 09:31:31 +0800

    clean up

--- public/static/api_config.js
+++ public/static/api_config.js
@@ -1,8 +1,8 @@
 var WGURL = {
     // 测试环境
     // api: 'http://999hadminapi.wgfoib4.com/manager/api/',
-    // api: 'http://127.0.0.1:8081/',
+    api: 'http://127.0.0.1:8081/',
     // api: 'http://10.0.0.2:8081/',
-    api: 'http://www.7dep.com:8081/',    
+    // api: 'http://www.7dep.com:8081/',    
     ws: 'http://999hadminws.wgfoib4.com/'
 }
